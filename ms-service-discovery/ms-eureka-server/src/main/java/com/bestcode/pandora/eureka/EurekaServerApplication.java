94e0d066a9c9e4d9852936d7fd0d55b54247d6f4
chuckie-xch <1205241831@qq.com>
2018-12-18 23:15:16 +0800

    project init

---
+++ ms-service-discovery/ms-eureka-server/src/main/java/com/bestcode/pandora/eureka/EurekaServerApplication.java
@@ -0,0 +1,22 @@
+package com.bestcode.pandora.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka server
+ *
+ * @author <a href="mailto:1205241831@qq.com">Xch</a>
+ * @version 1.00
+ * @see
+ * @since 2018.12.18
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
